1f774e352332ba961e1a018d306ad99a3e3a4058
katiafill <katiafill@mail.ru>
2024-02-06 13:37:56 +0700

    Initial commit

---
+++ src/main/java/ru/katiafill/airbookings/AirBookingsApplication.java
@@ -0,0 +1,11 @@
+package ru.katiafill.airbookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBookingsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirBookingsApplication.class, args);
+    }
+}
